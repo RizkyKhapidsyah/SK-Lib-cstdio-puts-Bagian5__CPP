68b8f4fc219956875bdfb447030619e8787d55e1
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-08-06 08:30:13 +0700

    Initial commit

---
+++ SK-Lib-cstdio-puts-Bagian5__CPP/Source.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <conio.h>
+
+/*
+	Source by AlphaCodingSkills (https://www.alphacodingskills.com)
+	Modified For Learn by RK
+	I.D.E : VS2022
+*/
+
+int main() {
+	char str[50] = "Halo Manusiaa";
+
+	puts(str);
+
+	_getch();
+	return 0;
+}
+
